7edb909690305f303debbb47e6bcfb9c0ca3cfc4
Janvier Zagabe <jzagabe@tfs.ca>
2020-04-21 08:45:50 -0400

    Initial Commit

---
+++ Calcul_IMC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calcul_IMC
+//
+//  Created by Janvier Rugomoka Zagabe on 2020-04-21.
+//  Copyright © 2020 Janvier Zagabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
